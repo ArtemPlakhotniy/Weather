a44eb270d3c4c575c178c1e04c50c2b85b6f950e
Minorius <minorius@mail.ru>
2015-11-19 18:34:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/minorius/weather/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.minorius.weather;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        new GetData().execute();
+    }
+
+    class GetData extends AsyncTask {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String result = "";
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?id=710791&appid=2de143494c0b295cca9337e1e96b00e0");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                result = buffer.toString();
+
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(), "Eror", Toast.LENGTH_SHORT).show();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            textView.setText(result);
+        }
+    }
+}
